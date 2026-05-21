a0d044a9186a8bb569e673c49b79784d725fd177
Vinayak <vinayak.reddy@wizergos.com>
2026-05-21 22:10:17 +0530

    initial commit - hello cicd app

---
+++ src/main/java/com/hellosamhita/hello_cicd/HelloCicdApplication.java
@@ -0,0 +1,13 @@
+package com.hellosamhita.hello_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCicdApplication.class, args);
+	}
+
+}
